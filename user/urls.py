53e94b4aa4100507b613a0131f9d20183912ebb7
sishen <zhangsishen02@gmail.com>
2024-04-04 12:20:08 +0100

    [登录登出]增加响应辅助函数#1

---
+++ user/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 
 # Create your views here.
+def index(req):
+    body = json.loads(req.body.decode("utf-8"))
+    return JsonResponse({"request": body["id"]})
